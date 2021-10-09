88abf1eea11da7da4c512f08d40aa55072103494
canaliyarman <53862337+canaliyarman@users.noreply.github.com>
2021-10-09 22:09:15 +0300

    Add files via upload

---
+++ jokeReader.py
@@ -0,0 +1,47 @@
+import json
+import pyttsx3
+import random
+import keyboard
+
+engine = pyttsx3.init()
+rate = engine.getProperty('rate')   # getting details of current speaking rate
+print (rate)                        #printing current voice rate
+engine.setProperty('rate', 170)
+
+f_reddit = open('reddit_jokes.json')
+j_reddit = json.load(f_reddit)
+f_stupidstuff = open('stupidstuff.json')
+j_ss = json.load(f_stupidstuff)
+
+while True:
+    if keyboard.read_key() == "j":        
+        flag = random.randint(0,5)
+        if flag == 0:
+            index = random.randint(0,len(j_ss))
+            while True:
+                if(j_ss[index]["rating"]<3):
+                    index = random.randint(0,len(j_reddit))
+                else:
+                    break
+            body = j_ss[index]["body"]
+            engine.say(body)
+            engine.runAndWait()
+            if keyboard.is_pressed("esc"):
+                engine.stop()
+        else:
+            index = random.randint(0,len(j_reddit))
+            while True:
+                if(j_reddit[index]["score"]<3):
+                    index = random.randint(0,len(j_reddit))
+                else:
+                    break
+            title = j_reddit[index]["title"]
+            body = j_reddit[index]["body"]
+            engine.say(title)
+            engine.runAndWait()
+            engine.say(body)
+            engine.runAndWait()
+            if keyboard.is_pressed("esc"):
+                engine.stop()
+            
+
